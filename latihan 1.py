eea8177052e5de7863504657d51acccd86f2681f
syafiralthf <azzahrasyafira06@gmail.com>
2024-10-21 19:07:43 +0700

    Add files via upload

---
+++ latihan 1.py
@@ -0,0 +1,16 @@
+# penggunaan end
+print('A', end='')
+print('b', end='')
+print('c', end='')
+print()
+print('x')
+print('y')
+print('z')
+
+# penggunaan separator
+w, x, y, z, =10, 15, 20, 25
+print(w, x, y, z,)
+print(w, x, y, z, sep=',')
+print(w, x, y, z, sep='')
+print(w, x, y, z, sep=':')
+print(w, x, y, z, sep='-----')
